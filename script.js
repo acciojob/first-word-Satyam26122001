146b96b4fdf6fa743265739278067e0e64dc5b43
Satyam26122001 <saryamking481@gmail.com>
2025-03-19 18:43:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading spaces to ignore them
+  s = s.trim();
+  
+  // Find the index of the first space
+  let spaceIndex = s.indexOf(' ');
+
+  // If there is no space, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring up to the first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
